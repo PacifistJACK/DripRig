239adc67863133221cc3fb8a12daaaa76088487a
Utkarsh Sharma <utkarshsharma3600@gmail.com>
2026-08-09 00:01:53 +0530

    as

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -11,7 +11,7 @@ import {
 
 const firebaseConfig = {
   apiKey: "AIzaSyBzfyOiXhCbqV3Qw-P6srvwWqt7OG7xX5k",
-  authDomain: "driprig.j4du.in",
+  authDomain: "driprig-383be.firebaseapp.com",
   projectId: "driprig-383be",
   storageBucket: "driprig-383be.firebasestorage.app",
   messagingSenderId: "269498335034",
